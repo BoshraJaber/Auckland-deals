043bee3768c207dfbcf994a0a795d40951895988
Boshra Jaber <boshrajaber94@gmail.com>
2022-05-26 15:56:00 +0300

    Add NavBar

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -1,13 +1,22 @@
+import React from 'react';
 import { useState } from 'react';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
-
 import { updateCity } from '../../store/reducers';
 import './Banner.css';
+import { ReactComponent as LocationIcon } from '../assets/map-pin.svg';
+import { ReactComponent as HeartIcon } from '../assets/heart.svg';
+import { ReactComponent as Mail } from '../assets/mail.svg';
+import { ReactComponent as ShoppingCart } from '../assets/shopping-cart.svg';
+import { ReactComponent as User } from '../assets/user.svg';
+
+
+
+
 
 export default function Banner() {
     const cities = ["Auckland", "Waikato", "Wellington", "Northland", "Manawatu - Wanganui", "Tauranga", "Rotorua - Taupo", "Hawkes Bay", "Christchurch", "Taranaki", "Nelson - Marlborough", "Queenstown - Wanaka", "Dunedin - Invercargill"];
-
+    const userOptions = ["My Account", "Gifts", "Profile", "My Subscriptions", "My Purchases", "Logout", "Login"]
     const [city, setCity] = useState("Auckland");
     const dispatch = useDispatch();
 
@@ -20,19 +29,50 @@ export default function Banner() {
     return (
         <header id="banner" role="banner">
             <Navbar className="Navbar" variant="dark" >
-                <Container className="d-flex">
-                    <Navbar.Brand href="/">GrabOne</Navbar.Brand>
+                <Container className="container">
+                    <Navbar.Brand href="/" className="nav-brand">GrabOne  </Navbar.Brand>
                     <Nav className="me-auto">
-                        <NavDropdown title={city} id="basic-nav-dropdown">
-                            {
-                                cities.map(city => {
-                                    return (
+                        <span className='droplist'>
+                            <LocationIcon />
+                            <NavDropdown title={city} id="basic-nav-dropdown">
 
-                                        <NavDropdown.Item onClick={() => { handleCityChange(city) }} >{city}</NavDropdown.Item>
-                                    )
-                                })
-                            }
-                        </NavDropdown>
+                                {
+                                    cities.map(city => {
+                                        return (
+
+                                            <NavDropdown.Item classNmae="black-text" onClick={() => { handleCityChange(city) }} >{city}</NavDropdown.Item>
+                                        )
+                                    })
+                                }
+                            </NavDropdown>
+                        </span>
+                        <span className='droplist'>
+                        <HeartIcon />
+                        </span>
+
+                        <span className='droplist'>
+                            <User />
+                            <NavDropdown id="basic-nav-dropdown">
+
+                                {
+                                    userOptions.map(option => {
+                                        return (
+
+                                            <NavDropdown.Item classNmae="black-text">{option}</NavDropdown.Item>
+                                        )
+                                    })
+                                }
+                            </NavDropdown>
+                        </span>
+                        <span className='droplist'>
+                            <ShoppingCart />
+
+                        </span>
+                        <span className='droplist'>
+
+                            <Mail />
+                            <p classNmae="second-text"> Subscribe</p>
+                        </span>
                     </Nav>
                 </Container>
             </Navbar>
